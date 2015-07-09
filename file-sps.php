e96a217952714e39c28dc88202abf5f71fa347f7
Yoshitaka Morimura <morimura.yoshitaka.3n@kyoto-u.ac.jp>
2015-07-09 12:26:15 +0900

    fix xml!!

--- file-sps.php
+++ file-sps.php
@@ -63,7 +63,7 @@ header('Content-Disposition: attachment; filename="kyodairaku2' . $ecsid . '.mob
 			  </array>
 			  <key>PayloadCertificateAnchorUUID</key>
 			  <string>034AE5DD-9456-40FB-81E5-8F3B8EF0F9EC</string>
-			  <key>TLSTrustedServerNames<key>
+			  <key>TLSTrustedServerNames</key>
 				  <array>
 					<string>kuins.kyoto-u.ac.jp</string>
 				  </array>
